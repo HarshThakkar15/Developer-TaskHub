3679694a4e8877bb0c5af60320cf2a90628388eb
HarshThakkar15 <167105422+HarshThakkar15@users.noreply.github.com>
2025-11-04 15:00:23 +0530

    final
    Removed static file serving and index.html route.

--- backend/server.js
+++ backend/server.js
@@ -22,12 +22,6 @@ app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/users", userRoutes);
 
-
-app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'));
-});
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
