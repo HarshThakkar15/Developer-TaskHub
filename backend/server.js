4b9ffd2a864eee5a612f14fcb78df7b5b138421c
HarshThakkar15 <thakkarharsh1508@gmail.com>
2025-11-04 14:44:01 +0530

    Final

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,11 @@ app.use("/api/tasks", taskRoutes);
 app.use("/api/users", userRoutes);
 
 
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
